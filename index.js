fdf9ccc2496e5f254be2271d85393c0d0f67b531
eSTee3 <tagsxa@yahoo.com>
2023-01-06 16:55:41 -0600

    Created output directory and added initial requirements to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Adds requirements for all libraries
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+// Add requirement for the inquirer and file system libraries
+const inquirer = require("inquirer");
+const fs = require("fs");
+// Add variables for files used in automation
