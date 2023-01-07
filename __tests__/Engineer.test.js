8cf1040221d4bd0cc4a7ab908efee5497d3a9da5
eSTee3 <tagsxa@yahoo.com>
2023-01-07 06:47:39 -0600

    Finishing up code and prepping for possible user-chosen team name

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require("../lib/Engineer");
 
-test("Create GitHub", () => {
+test("GitHub ID", () => {
     const githubId = "Spongy42";
     const employeeInstance = new Engineer("Spongebob", 42, "sbsp@bikinibottom.net", githubId);
     expect(employeeInstance.githubId).toBe(githubId);
 });
 
-test("Testing role.", () => {
+test("Test Role", () => {
     const returnValue = "Engineer";
     const employeeInstance = new Engineer("Spongebob", 42, "sbsp@bikinibottom.net", "Spongy42");
     expect(employeeInstance.getEmpRole()).toBe(returnValue);
