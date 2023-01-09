132a3860f20b854f2413a4ac252c3c842d37d161
eSTee3 <31021627+eSTee3@users.noreply.github.com>
2023-01-09 13:52:20 -0600

    Figured out how to implement Team name

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Adds requirements for all libraries
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
@@ -13,6 +12,8 @@ const teamTemplate = require("./src/teamTemplate.js");
 
 // Create array with all entered information to be used in the creation of the HTML page
 empArray = [];
+// Create an array (of a single item) to be used in the naming of the file
+fileName = [];
 
 
 function runApplication() {
@@ -22,28 +23,22 @@ function runApplication() {
         inquirer.prompt([{
 
             type: "list",
-            message: "Please choose the type of employee you're adding to the team:",
+            message: "Does your team have a Manager?",
             name: "empType",
-            choices: ["Engineer", "Intern", "Manager", "I'm done!"]
+            choices: ["Yes, there's a Manager", "No, there's not a Manager"]
 
         }]).then(function (userInput) {
             switch(userInput.empType) {
-                case "Manager":
+                case "Yes, there's a Manager":
                     newMgr();
-                    break;
-                case "Engineer":
-                    newEng();
-                    break;
-                case "Intern":
-                    newInt();
                     break;
                 // This will create the HTML if something other than "Engineer", "Intern" or "Manager" is chosen.
                 default:
-                    createHTML();
+                    buildRestOfTeam();
             }
         })
     }
-    // Function used, if Manager is selected first (it excludes manager from the list of options)
+    // Function used, if Manager is selected (it excludes manager from the future list of options)
     function buildRestOfTeam() {
         inquirer.prompt([{
 
@@ -62,7 +57,7 @@ function runApplication() {
                     break;
                 // This will create the HTML if Engineer, Intern or Manager are not chosen.
                 default:
-                    createHTML();
+                    teamName();
             }
         })
     }
@@ -124,7 +119,7 @@ function runApplication() {
         ]).then(detail => {
             const engineer = new Engineer(detail.engName, detail.engId, detail.engEmail, detail.engGitHub);
             empArray.push(engineer);
-            buildTeam();
+            buildRestOfTeam();
         });
     }
 
@@ -154,19 +149,35 @@ function runApplication() {
         ]).then(detail => {
             const intern = new Intern(detail.intName, detail.intId, detail.intEmail, detail.intSchool);
             empArray.push(intern);
-            buildTeam();
+            buildRestOfTeam();
         });
     }
 
+    // Function to get the team's name from the end user to use in the file naming and web page header!!!!
+    function teamName() {
+        inquirer.prompt([{
+            type: "input",
+            name: "file",
+            message: "Please Enter a Name for this Team:"
+        }
+
+    ]).then(detail => {
+        const filename = (detail.file);
+        fileName.push(filename);
+        createHTML();
+    });
+    }
+    
 
     // Function that creates the HTML file (utilizing the teample) and then notifies the end user of it's completion and it's location
     function createHTML() {
         
-        fs.writeFileSync("./dist/MyTeam.html", teamTemplate(empArray), "UTF-8");
+        fs.writeFileSync(`./dist/${fileName}.html`, teamTemplate(empArray), "UTF-8");
         console.log("-------------------------------------------");
         console.log("Thank you!, your team page has been created.");
         console.log("It's been coded into a file called");
-        console.log("'MyTeam.html' within the 'dist' folder.");
+        // Names the file using the end user's input for team name
+        console.log(`'${fileName}.html' within the 'dist' folder.`);
     }
 
     buildTeam();
